c21f19500011bc9619db2e435761fb817a948ab2
cassiusbessa <cassiusbessa@gmail.com>
2023-02-23 00:05:15 -0300

    feat: :sparkles: create new social media icons

--- src/screens/intro/index.tsx
+++ src/screens/intro/index.tsx
@@ -3,12 +3,14 @@ import CatchPhrase from '@components/catchphrase';
 import IntroNavBar from '@components/intro-nav-bar';
 import Logo from '@components/logo';
 import './styles.scss';
+import SocialMediaIcons from '@components/social-media-icons';
 const Intro = () => (
 	<div className='intro-container'>
 		<div className='intro-content-container'>
 			<Logo />
 			<CatchPhrase />
 			<IntroNavBar />
+			<SocialMediaIcons />
 		</div>
 	</div>
 );
