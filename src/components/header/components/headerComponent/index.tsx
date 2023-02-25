01011be8e53ea174bba5f28422d8c79021f4af9c
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-02-24 22:25:38 -0300

    feat: create contant to for color

--- src/components/header/components/headerComponent/index.tsx
+++ src/components/header/components/headerComponent/index.tsx
@@ -1,11 +1,12 @@
 import logo from '@assets/intro/logo.svg';
 import SvgHamburguerIcon from '@assets/header/SvgHamburguerIcon';
+import {primaryDarkGreen} from '@styles/inline-colors';
 import './style.scss';
 
 const HeaderComponent = () => (
 	<header className='header'>
 		<img src={logo} alt='Logo Icon' height='100%' />
-		<SvgHamburguerIcon color='red' />
+		<SvgHamburguerIcon onClickColor={primaryDarkGreen} />
 	</header>
 );
 
