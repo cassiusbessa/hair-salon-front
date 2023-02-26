d08efe78968d0687bfd430592b9f443a598c1fe4
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-02-26 18:05:16 -0300

    feat: apply resize to main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {useState} from 'react';
 import {store} from '@store/ReduxStore';
 import {Provider} from 'react-redux';
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,17 +6,20 @@ import './styles.scss';
 
 const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [resideUp, setresideUp] = useState(false);
 
 	const domNode = useClickOutSide(() => {
+		setresideUp(false);
 		setIsOpen(false);
 	}) as LegacyRef<HTMLDivElement>;
 
 	const handlerClickHamburguer = () => {
+		setresideUp(!isOpen);
 		setIsOpen(!isOpen);
 	};
 
 	return (
-		<div className='header-dropdown-container' ref={domNode}>
+		<div className={`header-dropdown-container ${resideUp ? 'resize-up' : ''}`} ref={domNode}>
 			<HeaderComponent onClickHamburguer={handlerClickHamburguer} />
 			<DropdownComponent isOpen={isOpen}/>
 		</div>
