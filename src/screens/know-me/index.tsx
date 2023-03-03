a30cc912aa37f959a90c995dde78184e9dfa1576
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-03-03 14:40:45 -0300

    feat: add responsive knowme component for desktop

--- src/screens/know-me/index.tsx
+++ src/screens/know-me/index.tsx
@@ -2,13 +2,13 @@ import sulaPhoto from '@assets/know-me/sula-photo.png';
 import './styles.scss';
 
 const KnowMe = () => (
-	<div className='know-me-container'>
-		<h1>Conheça-me</h1>
+	<section className='know-me-container'>
+		<h2>Conheça-me</h2>
 		<div className='know-me-content'>
 			<img src={sulaPhoto} alt='Sula' />
 			<p> Aqui nós iremos escrever dois parágrafos sobre a dona do salão. Contar sobre qualidades, cursos, experiencias e etc </p>
 		</div>
-	</div>
+	</section>
 );
 
 export default KnowMe;
