75a4ea617948eb615c949e2f6583bcd010fb6902
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-03-10 23:06:45 -0300

    refactor: rename redux store file

--- src/redux/store/redux-store.ts
+++ src/redux/store/redux-store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from '@states/count';
+import carouselImagesReducer from '@states/carousel-images';
 
 export const store = configureStore({
 	reducer: {
 		counter: counterReducer,
+		carouselImages: carouselImagesReducer,
 	},
 });
 
