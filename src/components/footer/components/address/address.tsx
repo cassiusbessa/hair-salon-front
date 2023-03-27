182755c9126bc40cb28695921f49bb0de3eeac8a
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-03-27 18:03:10 -0300

    refactor: remove street and cep

--- src/components/footer/components/address/address.tsx
+++ src/components/footer/components/address/address.tsx
@@ -4,10 +4,10 @@ const Address = () => {
 	return (
 		<div className='footer-address'>
 			<p>Endereço:</p>
-			<p>Rua Sá Ferreira, 44</p>
+			{/* <p>Rua Sá Ferreira, 44</p> */}
 			<p>Copacabana</p>
 			<p>Rio de Janeiro/RJ</p>
-			<p>CEP: 22071-100</p>
+			{/* <p>CEP: 22071-100</p> */}
 		</div>
 	);
 };
