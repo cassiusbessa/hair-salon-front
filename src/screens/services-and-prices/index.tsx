4566c323019abc69183059046c7b9948442803d3
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-03-27 18:12:20 -0300

    refactor: rewrite service info from services-and-prices component

--- src/screens/services-and-prices/index.tsx
+++ src/screens/services-and-prices/index.tsx
@@ -4,29 +4,36 @@ import {useWindowWidthResize} from '@custom-hooks/use-window-width';
 
 const serviceInfo: DropdownServiceRequirements[] = [
 	{
+		id: '0',
+		content: 'Tenha um cabelo liso e brilhante com nossos alisamentos exclusivos e de alta qualidade.',
+		price: '100,00',
+		subTitle: 'Alisamento',
+		title: 'Alisamento',
+	},
+	{
 		id: '1',
-		content: 'Cortes modernos, exclusivos e de qualidade',
+		content: 'Experimente nossos cortes modernos e exclusivos, personalizados para realçar sua beleza natural.',
 		price: '100,00',
 		subTitle: 'Cortes',
 		title: 'Cortes',
 	},
 	{
 		id: '2',
-		content: 'Colorações modernas, exclusivas e de qualidade',
+		content: 'Renove seu visual com nossas colorações modernas e de qualidade, que trazem vida aos seus cabelos.',
 		price: '100,00',
 		subTitle: 'Coloração',
 		title: 'Coloração',
 	},
 	{
 		id: '3',
-		content: 'Hidratações modernas, exclusivas e de qualidade',
+		content: 'Deixe seus cabelos hidratados e saudáveis com nossas hidratações exclusivas e de alta qualidade.',
 		price: '100,00',
 		subTitle: 'Hidratação',
 		title: 'Hidratação',
 	},
 	{
 		id: '4',
-		content: 'Lavagens modernas, exclusivas e de qualidade',
+		content: 'Sinta a diferença em seus cabelos com nossas lavagens modernas e de qualidade, que limpam e cuidam dos seus fios.',
 		price: '100,00',
 		subTitle: 'Lavagem',
 		title: 'Lavagem',
