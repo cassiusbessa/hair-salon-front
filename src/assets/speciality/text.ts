0c2807629b8c5452e03df1851f8533cb4bacd71f
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-03-27 22:24:44 -0300

    refactor: add new text to especialidade

--- src/assets/speciality/text.ts
+++ src/assets/speciality/text.ts
@@ -1,5 +1,5 @@
 export const title = 'Especialidade';
-export const text = 'Nosso salão é especializado em cortes de cabelo, hidratação e tintura, com uma equipe de profissionais altamente qualificados prontos para transformar o seu cabelo em uma obra de arte. Venha nos visitar e descubra como podemos ajudá-lo a ter o cabelo dos seus sonhos!';
+export const text = 'Sula é uma especialista altamente qualificada e experiente na indústria de beleza, com um foco especial na arte de alisamento capilar. Com uma vasta experiência em trabalhar com todos os tipos de cabelo e técnicas inovadoras, capaz de proporcionar um alisamento perfeitamente liso e impecável. Além disso, sendo especializada em outros serviços, como corte, escovas, tratamentos e hidratação capilar. Sempre pronta para ajudá-lo a alcançar o cabelo dos seus sonhos. Venha nos visitar e descubra como podemos transformar seu cabelo em uma verdadeira obra de arte.';
 
 
 
