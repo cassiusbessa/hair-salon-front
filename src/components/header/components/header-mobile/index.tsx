bace6f0d70c3cf6e1291796f6627747a1ef88b02
cassius <cassiusbessa@gmail.com>
2023-12-11 20:41:37 -0300

    refactor: :art: change alts images descriptrion but, inutil

--- src/components/header/components/header-mobile/index.tsx
+++ src/components/header/components/header-mobile/index.tsx
@@ -1,22 +1,22 @@
 import logo from '@assets/intro/logo.png';
-import {primaryDarkGreen} from '@styles/inline-colors';
+import { primaryDarkGreen } from '@styles/inline-colors';
 import SvgHamburguerIcon from '@assets/header/svg-hamburguer-icon';
 import './style.scss';
 
 type Props = {
-	onClickHamburguer?: () => void;
-	myRef?: React.LegacyRef<HTMLElement> | undefined;
-	onScroll?: React.UIEventHandler<HTMLElement> | undefined;
-	style?: React.CSSProperties | undefined;
+  onClickHamburguer?: () => void;
+  myRef?: React.LegacyRef<HTMLElement> | undefined;
+  onScroll?: React.UIEventHandler<HTMLElement> | undefined;
+  style?: React.CSSProperties | undefined;
 };
 
-const HeaderMobile = ({onClickHamburguer, myRef, onScroll, style}: Props) => (
-	<header className='header-mobile' onScroll={onScroll} ref={myRef} style={style}>
-		<img src={logo} alt='Logo Icon' height='100%' />
-		<button onClick={onClickHamburguer}>
-			<SvgHamburguerIcon onClickColor={primaryDarkGreen} />
-		</button>
-	</header>
+const HeaderMobile = ({ onClickHamburguer, myRef, onScroll, style }: Props) => (
+  <header className='header-mobile' onScroll={onScroll} ref={myRef} style={style}>
+    <img loading='lazy' src={logo} alt='Logo Icon' height='100%' />
+    <button onClick={onClickHamburguer}>
+      <SvgHamburguerIcon onClickColor={primaryDarkGreen} />
+    </button>
+  </header>
 );
 
 export default HeaderMobile;
--- src/components/our-works/index.tsx
+++ src/components/our-works/index.tsx
@@ -1,49 +1,49 @@
 import Carousel from '@components/carousel';
 import './styles.scss';
 import CarouselImageItem from '@components/carousel-image-item';
-import {useSelector} from 'react-redux';
-import {incrementCurrentItem, decrementCurrentItem, setMaxItems, setIsFrozen, setCurrentItem} from '@states/our-works-reducer';
-import {type RootState} from '@store/redux-store';
+import { useSelector } from 'react-redux';
+import { incrementCurrentItem, decrementCurrentItem, setMaxItems, setIsFrozen, setCurrentItem } from '@states/our-works-reducer';
+import { type RootState } from '@store/redux-store';
 import CarouselDots from '@components/carousel-dots';
-import carousel1 from '@assets/our-works/carousel1.jpg';
-import carousel2 from '@assets/our-works/carousel2.jpg';
-import carousel3 from '@assets/our-works/carousel3.jpg';
-import carousel4 from '@assets/our-works/carousel4.jpg';
-import carousel5 from '@assets/our-works/carousel5.jpeg';
+import carousel1 from '@assets/our-works/loiro-reluzente.jpg';
+import carousel2 from '@assets/our-works/restauracao-escova.jpg';
+import carousel3 from '@assets/our-works/cabelo-ruivo.jpg';
+import carousel4 from '@assets/our-works/pintura-capilar.jpeg';
+import carousel5 from '@assets/our-works/cabelo-colorido.jpg';
 
 const items = [
-	{content: carousel1, alt: 'Haircut Image'},
-	{content: carousel2, alt: 'Haircut Image'},
-	{content: carousel3, alt: 'Haircut Image'},
-	{content: carousel4, alt: 'Haircut Image'},
-	{content: carousel5, alt: 'Haircut Image'},
+  { content: carousel1, alt: 'Escova em Cabelo Loiro Reluzente/Brilhante' },
+  { content: carousel2, alt: 'Antes e Depois de Restruturação, Hidratação e Tratamento em cabelo danificado por Química' },
+  { content: carousel3, alt: 'Pintura de cabelo Ruivo/Vermelho' },
+  { content: carousel4, alt: 'Pintura capilar em vemelho forte' },
+  { content: carousel5, alt: 'Cabelo Colorido de diversas cores' },
 ];
 
 const OurWorks = () => {
-	const ourWorksStates = useSelector((state: RootState) => state.ourWorks);
-	const ourWorksActions = {
-		incrementCurrentItem,
-		decrementCurrentItem,
-		setMaxItems,
-		setIsFrozen,
-		setCurrentItem,
-	};
+  const ourWorksStates = useSelector((state: RootState) => state.ourWorks);
+  const ourWorksActions = {
+    incrementCurrentItem,
+    decrementCurrentItem,
+    setMaxItems,
+    setIsFrozen,
+    setCurrentItem,
+  };
 
-	return (
-		<div className='our-works-carousel box-shadow' id='our-works'>
-			<Carousel
-				Element={CarouselImageItem}
-				states={ourWorksStates}
-				actions={ourWorksActions}
-				items={items}
-				style={{height: '80%'}}
-			/>
-			<div className='carousel-images-footer'>
-				<h2>Nossos trabalhos</h2>
-				<CarouselDots actions={ourWorksActions} states={ourWorksStates} />
-			</div>
-		</div>
-	);
+  return (
+    <div className='our-works-carousel box-shadow' id='our-works'>
+      <Carousel
+        Element={CarouselImageItem}
+        states={ourWorksStates}
+        actions={ourWorksActions}
+        items={items}
+        style={{ height: '80%' }}
+      />
+      <div className='carousel-images-footer'>
+        <h2>Nossos trabalhos</h2>
+        <CarouselDots actions={ourWorksActions} states={ourWorksStates} />
+      </div>
+    </div>
+  );
 };
 
 export default OurWorks;
